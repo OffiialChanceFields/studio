844b96e20ca6be9cd1661ee771217604a77a2e9c
Chance Gibson <chanceg453@gmail.com>
2025-08-09 21:21:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-import { tailwindExtension } from './src/lib/theme/goldTheme.mjs';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  theme: {
-    extend: tailwindExtension,
-  },
+  
 };
 
 export default nextConfig;
