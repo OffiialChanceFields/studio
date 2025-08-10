8d5b8409509c634293b1075c88b43c4751164f1a
Chance Gibson <chanceg453@gmail.com>
2025-08-10 08:18:17 +0000

    Sync changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  devIndicators: {
+    allowedDevOrigins: [
+      "9003-firebase-studio-1754771873430.cluster-2xid2zxbenc4ixa74rpk7q7fyk.cloudworkstations.dev",
+    ],
+  },
 };
 
 export default nextConfig;
