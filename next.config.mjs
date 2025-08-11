2c85c403f623f206af9e7fad40c6a4e70136acc1
Chance Gibson <chanceg453@gmail.com>
2025-08-11 10:46:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: {
-    allowedDevOrigins: [
-      "9003-firebase-studio-1754771873430.cluster-2xid2zxbenc4ixa74rpk7q7fyk.cloudworkstations.dev",
-    ],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
   },
 };
 
